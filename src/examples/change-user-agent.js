39612fd10b948481f7f7ce3d1b0be83ba4839c2a
Rongjian Zhang <pd4d10@gmail.com>
2018-07-07 17:45:14 +0800

    Update examples, remove name

--- src/examples/change-user-agent.js
+++ src/examples/change-user-agent.js
@@ -1,25 +1,30 @@
+/**
+ * Change all requests' User-Agent to iOS Safari
+ *
+ * For more information, see:
+ * https://developer.chrome.com/extensions/webRequest
+ * https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/webRequest
+ */
+
 const myUserAgent =
   'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
 
 export default {
-  name: 'Change User Agent',
-  payload: {
-    lifecycle: 'onBeforeSendHeaders',
-    context: ({ requestHeaders }) => {
-      for (var i = 0; i < requestHeaders.length; ++i) {
-        if (requestHeaders[i].name.toLowerCase() === 'user-agent') {
-          requestHeaders[i].value = myUserAgent
-          break
-        }
+  lifecycle: 'onBeforeSendHeaders',
+  callback: ({ requestHeaders }) => {
+    for (var i = 0; i < requestHeaders.length; ++i) {
+      if (requestHeaders[i].name.toLowerCase() === 'user-agent') {
+        requestHeaders[i].value = myUserAgent
+        break
       }
-      return { requestHeaders }
-    },
-    filter: {
-      urls: ['<all_urls>'],
-    },
-    options: [
-      'requestHeaders',
-      'blocking', // Add `blocking` here since we want to change request headers
-    ],
+    }
+    return { requestHeaders }
   },
+  filter: {
+    urls: ['<all_urls>'],
+  },
+  options: [
+    'requestHeaders',
+    'blocking', // Add `blocking` here since we want to change request headers
+  ],
 }
---
+++ src/examples/remove-utm-tokens.js
@@ -0,0 +1,42 @@
+/**
+ * Remove Google Analytics UTM tokens
+ * https://github.com?utm_source=xxx&utm_medium=email -> https://github.com
+ *
+ * For more information, see:
+ * https://developer.chrome.com/extensions/webRequest
+ * https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/webRequest
+ */
+
+export default {
+  lifecycle: 'onBeforeRequest',
+  callback: details => {
+    const url = new URL(details.url)
+    const keys = url.searchParams.keys()
+    const keysToBeDeleted = []
+    for (const key of keys) {
+      if (key.startsWith('utm_')) {
+        keysToBeDeleted.push(key)
+      }
+    }
+    keysToBeDeleted.forEach(key => {
+      url.searchParams.delete(key)
+    })
+
+    // Only redirect if the new URL is different with the old
+    const newUrl = url.toString()
+    if (newUrl != details.url) {
+      return { redirectUrl: newUrl }
+    }
+  },
+  filter: {
+    urls: ['<all_urls>'],
+    types: [
+      // Only do removal at main frame,
+      // because we don't care URL of XHR, images or any other resources
+      'main_frame',
+    ],
+  },
+  options: [
+    'blocking', // Add `blocking` here so we can do redirect
+  ],
+}
