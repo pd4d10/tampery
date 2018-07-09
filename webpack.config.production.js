662155e4caf0925fbb45372e0b0b3c58c3669b17
Rongjian Zhang <pd4d10@gmail.com>
2018-07-09 22:19:34 +0800

    Add icon, add production build config

---
+++ webpack.config.production.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const config = require('./webpack.config')
+
+module.exports = {
+  ...config,
+  watch: false,
+  mode: 'production',
+  devtool: false,
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+    ],
+  },
+}
