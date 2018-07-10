c0220c9ed680697ee490243f837025de9646f4e5
Rongjian Zhang <pd4d10@gmail.com>
2018-07-10 20:08:31 +0800

    Update About page

--- src/dashboard/about.js
+++ src/dashboard/about.js
@@ -2,10 +2,17 @@ import React from 'react'
 
 const About = () => (
   <div>
+    <p>Tampery is an open source project.</p>
     <p>
-      Source code:{' '}
+      Source code here:{'  '}
       <a href="https://github.com/pd4d10/tampery" target="_blank">
         https://github.com/pd4d10/tampery
+      </a>
+    </p>
+    <p>
+      If something went wrong, please{' '}
+      <a href="https://github.com/pd4d10/tampery/issues/new" target="_blank">
+        submit an issue
       </a>
     </p>
   </div>
