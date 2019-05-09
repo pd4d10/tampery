a05e88d87fc93c73265499b243101d89dba7e7cf
Rongjian Zhang <pd4d10@gmail.com>
2019-05-10 00:16:31 +0800

    Revert "refactor: replace antd components of App"
    This reverts commit 9ae30a4e64d1cdeeb3eda36d589e0e77ba37467c.

--- src/dashboard/app.tsx
+++ src/dashboard/app.tsx
@@ -1,71 +1,71 @@
+import { HashRouter, Route, Link, withRouter } from 'react-router-dom'
 import React from 'react'
-import { HashRouter, Route } from 'react-router-dom'
-import {
-  Container,
-  Dropdown,
-  DropdownItem,
-  Button,
-  Navbar,
-  NavRouterLink,
-  NavbarBrand,
-} from 'relaunch'
+import { Layout, Menu, Dropdown, Button } from 'antd'
 
 import { About } from './about'
 import { Home } from './home'
 import { Edit } from './edit'
 
-import style from 'bootstrap/dist/css/bootstrap.min.css'
+import style from 'antd/dist/antd.min.css'
 import { examples } from './utils'
 
 style // This is a hack for preventing Webpack drop CSS at production mode
+const { Content } = Layout
+
+const AddScriptButton = withRouter(props => (
+  <Dropdown
+    overlay={
+      <Menu>
+        {examples.map((example, index) => (
+          <Menu.Item key={example.name}>
+            <a
+              href="#"
+              onClick={async e => {
+                e.preventDefault()
+                props.history.push(`/add/${index}`)
+              }}
+            >
+              {example.name}
+            </a>
+          </Menu.Item>
+        ))}
+      </Menu>
+    }
+    placement="bottomLeft"
+  >
+    <Button type="primary">Add script</Button>
+  </Dropdown>
+))
 
 export const App = () => {
   return (
     <HashRouter>
-      <Container style={{ minHeight: '100%' }}>
-        <Route>
-          {({ history }) => (
-            <Navbar
-              theme="light"
-              bg="light"
-              brand={<NavbarBrand>Tampery</NavbarBrand>}
-              right={
-                <Dropdown
-                  text="Add script"
-                  overlay={examples.map((example, index) => (
-                    <DropdownItem
-                      key={example.name}
-                      onClick={async e => {
-                        e.preventDefault()
-                        history.push(`/add/${index}`)
-                      }}
-                    >
-                      {example.name}
-                    </DropdownItem>
-                  ))}
-                  placement="bottomLeft"
-                >
-                  <Button theme="primary">Add script</Button>
-                </Dropdown>
-              }
-            >
-              <NavRouterLink exact to="/">
-                Home
-              </NavRouterLink>
-              <NavRouterLink to="/about">About</NavRouterLink>
-            </Navbar>
-          )}
-        </Route>
-
-        <div style={{ padding: '20px 50px 0' }}>
-          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-            <Route path="/" exact component={Home} />
-            <Route path="/edit/:id" component={Edit} />
-            <Route path="/add/:index" component={Edit} />
-            <Route path="/about" component={About} />
+      <>
+        <Layout style={{ minHeight: '100%' }}>
+          <div style={{ background: '#fff', padding: '0 50px' }}>
+            <div className="logo" />
+            <Menu mode="horizontal" defaultSelectedKeys={['/']}>
+              <Menu.Item key="/">
+                <Link to="/">Home</Link>
+              </Menu.Item>
+              <Menu.Item key="/about">
+                <Link to="/about">About</Link>
+              </Menu.Item>
+              <Menu.Item key="/add" style={{ float: 'right' }}>
+                <AddScriptButton />
+              </Menu.Item>
+            </Menu>
           </div>
-        </div>
-      </Container>
+          <Content style={{ padding: '20px 50px 0' }}>
+            <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+              <Route path="/" exact component={Home} />
+              <Route path="/edit/:id" component={Edit} />
+              <Route path="/add/:index" component={Edit} />
+              <Route path="/about" component={About} />
+            </div>
+          </Content>
+        </Layout>
+      </>
     </HashRouter>
   )
 }
