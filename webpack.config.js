ad0e9d11dcd458eb3a31a9f6163c9eb3a9c5ebd7
Rongjian Zhang <pd4d10@gmail.com>
2020-01-11 11:37:27 +0800

    fix: ttf file loader

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,10 @@ const config = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.ttf$/,
+        use: ['file-loader'],
+      },
     ],
   },
   node: {
