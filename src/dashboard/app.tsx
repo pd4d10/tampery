55ad04d7c56f16b9924442f8921750b978594604
Rongjian Zhang <pd4d10@gmail.com>
2024-11-19 22:17:12 +0800

    refactor: new menu props

--- src/dashboard/app.tsx
+++ src/dashboard/app.tsx
@@ -31,34 +31,34 @@ export const App: React.FC = () => {
   return (
     <DataProvider>
       <HashRouter>
-        <>
-          <Layout style={{ minHeight: "100%" }}>
-            <div style={{ background: "#fff", padding: "0 50px" }}>
-              <div className="logo" />
-              <Menu mode="horizontal" defaultSelectedKeys={["/"]}>
-                <Menu.Item key="/">
-                  <Link to="/">Home</Link>
-                </Menu.Item>
-                <Menu.Item key="/about">
-                  <Link to="/about">About</Link>
-                </Menu.Item>
-                <Menu.Item key="/add" style={{ float: "right" }}>
-                  <AddScriptButton />
-                </Menu.Item>
-              </Menu>
+        <Layout style={{ minHeight: "100%" }}>
+          <div style={{ background: "#fff", padding: "0 50px" }}>
+            <div className="logo" />
+            <Menu
+              mode="horizontal"
+              defaultSelectedKeys={["/"]}
+              items={[
+                { key: "/", label: <Link to="/">Home</Link> },
+                { key: "/about", label: <Link to="/about">About</Link> },
+                {
+                  key: "/add",
+                  label: <AddScriptButton />,
+                  style: { float: "right" },
+                },
+              ]}
+            />
+          </div>
+          <Layout.Content style={{ padding: "20px 50px 0" }}>
+            <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/edit/:id" element={<Edit />} />
+                <Route path="/add/:id" element={<Edit />} />
+                <Route path="/about" element={<About />} />
+              </Routes>
             </div>
-            <Layout.Content style={{ padding: "20px 50px 0" }}>
-              <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/edit/:id" element={<Edit />} />
-                  <Route path="/add/:id" element={<Edit />} />
-                  <Route path="/about" element={<About />} />
-                </Routes>
-              </div>
-            </Layout.Content>
-          </Layout>
-        </>
+          </Layout.Content>
+        </Layout>
       </HashRouter>
     </DataProvider>
   );
